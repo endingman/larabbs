731c84eaf3aecff04ddd053fb7765913ccddaaaf
endingman <631925002@qq.com>
2018-05-22 01:31:51 +0000

    Seeding topics and users data

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -3,7 +3,18 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Topic::class, function (Faker $faker) {
+    $sentence = $faker->sentence();
+
+    /**随机一个月内的时间**/
+    $updated_at = $faker->dateTimeThisMonth();
+    /**创建时间要比更新时间早**/
+    $created_at = $faker->dateTimeThisMonth($updated_at);
+
     return [
-        // 'name' => $faker->name,
+        'title'      => $sentence, //sentence小段落生成
+        'body'       => $faker->text(), //text()大段落生成
+        'excerpt'    => $sentence,
+        'created_at' => $created_at,
+        'updated_at' => $updated_at,
     ];
 });
--- database/seeds/TopicsTableSeeder.php
+++ database/seeds/TopicsTableSeeder.php
@@ -1,20 +1,38 @@
 <?php
 
-use Illuminate\Database\Seeder;
+use App\Models\Category;
 use App\Models\Topic;
+use App\Models\User;
+use Illuminate\Database\Seeder;
 
 class TopicsTableSeeder extends Seeder
 {
     public function run()
     {
-        $topics = factory(Topic::class)->times(50)->make()->each(function ($topic, $index) {
-            if ($index == 0) {
-                // $topic->field = 'value';
-            }
-        });
+        //所有用户id数组[1,2,3,4...]
+        $user_ids = User::all()->pluck('id')->toArray();
+
+        //所有分类id数组[1,2,3,4...]
+        $category_ids = Category::all()->pluck('id')->toArray();
+
+        //faker实例
+        $faker = app(Faker\Generator::class);
+
+        $topics = factory(Topic::class)
+            ->times(100)
+            ->make()
+            ->each(function ($topic, $index)
+                 use ($user_ids, $category_ids, $faker) {
+
+                    //从用户id中随机取填充
+                    $topic->user_id = $faker->randomElement($user_ids);
+
+                    //分类同上
+                    $topic->category_id = $faker->randomElement($category_ids);
+
+                });
 
         Topic::insert($topics->toArray());
     }
 
 }
-
