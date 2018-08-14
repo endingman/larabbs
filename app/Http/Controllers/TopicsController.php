1d05961b9edc52a54c35ffc12c1f7eaeac975b8e
endingman <631925002@qq.com>
2018-08-14 01:47:28 +0000

    Topic order

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
 use App\Models\Topic;
+use Illuminate\Http\Request;
 
 class TopicsController extends Controller
 {
@@ -13,9 +14,9 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-    public function index()
+    public function index(Request $request, Topic $topic)
     {
-        $topics = Topic::with('user', 'category')->paginate(30);
+        $topics = $topic->withOrder($request->order)->paginate(20);
         return view('topics.index', compact('topics'));
     }
 
--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -15,4 +15,37 @@ class Topic extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function scopeWithOrder($query, $order)
+    {
+        switch ($order) {
+            case 'recent':
+                $query->recent();
+                break;
+            default:
+                $query->recentReplied();
+                break;
+        }
+
+        // 预加载防止 N+1 问题
+        return $query->with('user', 'category');
+    }
+
+    public function scopeRecentReplied($query)
+    {
+        // 当话题有新回复时，我们将编写逻辑来更新话题模型的 reply_count 属性，
+        // 此时会自动触发框架对数据模型 updated_at 时间戳的更新
+        return $query->orderBy('updated_at', 'desc');
+    }
+
+    public function scopeRecent($query)
+    {
+        // 按照创建时间排序
+        return $query->orderBy('created_at', 'desc');
+    }
+
+    /**
+     * 这里我们使用了Laravel本地作用域。本地作用域允许我们定义通用的约束集合以便在应用中复用。要定义这样的一个作用域，只需简单在对应 Eloquent 模型方法前加上一个scope前缀，作用域总是返回查询构建器。一旦定义了作用域，则可以在查询模型时调用作用域方法。在进行方法调用时不需要加上 scope 前缀。如以上代码中的 recent() 和 recentReplied()。
+     */
+
 }
