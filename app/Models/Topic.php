147956a39a33c734b4d80e8c99c7c2c75253a5d5
endingman <631925002@qq.com>
2018-08-22 08:38:42 +0000

    Auto translate slug

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -48,4 +48,9 @@ class Topic extends Model
      * 这里我们使用了Laravel本地作用域。本地作用域允许我们定义通用的约束集合以便在应用中复用。要定义这样的一个作用域，只需简单在对应 Eloquent 模型方法前加上一个scope前缀，作用域总是返回查询构建器。一旦定义了作用域，则可以在查询模型时调用作用域方法。在进行方法调用时不需要加上 scope 前缀。如以上代码中的 recent() 和 recentReplied()。
      */
 
+    public function link($params = [])
+    {
+        return route('topics.show', array_merge([$this->id, $this->slug], $params));
+    }
+
 }
