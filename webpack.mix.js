82804c5c5805e73cad20b83fad8b60a24a6adcd4
endingman <631925002@qq.com>
2018-08-22 06:58:45 +0000

    WYSIWYG Editor

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,10 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css').version();
+   .sass('resources/assets/sass/app.scss', 'public/css').version()
+   .copyDirectory('resources/assets/editor/js', 'public/js')
+   .copyDirectory('resources/assets/editor/css', 'public/css')
+   ;
 
 
 mix.browserSync({
