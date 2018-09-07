133a666ae4db4df9c7ec10474e696240666ac97b
endingman <631925002@qq.com>
2018-09-07 08:09:01 +0000

    api/user

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Requests\Api\UserRequest;
 use App\Models\User;
+use App\Transformers\UserTransformer;
 
 class UsersController extends Controller
 {
@@ -28,5 +29,10 @@ class UsersController extends Controller
         \Cache::forget($request->verification_key);
 
         return $this->response->created();
+    }
+
+    public function me()
+    {
+        return $this->response->item($this->user, new UserTransformer());
     }
 }
