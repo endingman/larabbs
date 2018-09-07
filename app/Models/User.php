c62015fc457a05db101185c830a5bb52b7dc4431
endingman <631925002@qq.com>
2018-09-07 02:50:40 +0000

    add jwt

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,9 @@ use Auth;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
 
     use Notifiable {
@@ -93,5 +94,17 @@ class User extends Authenticatable
         }
 
         $this->attributes['avatar'] = $path;
+    }
+
+    // Rest omitted for brevity
+
+    function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    function getJWTCustomClaims()
+    {
+        return [];
     }
 }
