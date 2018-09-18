27e985d253f8ca06294c4e59f1a69d58f6d30111
endingman <631925002@qq.com>
2018-09-18 07:33:51 +0000

    push notification

--- app/Http/Controllers/Api/Controller.php
+++ app/Http/Controllers/Api/Controller.php
@@ -8,4 +8,9 @@ use Dingo\Api\Routing\Helpers;
 class Controller extends BaseController
 {
     use Helpers;
+
+    public function errorResponse($statusCode, $message = null, $code = 0)
+    {
+        throw new HttpException($statusCode, $message, null, [], $code);
+    }
 }
